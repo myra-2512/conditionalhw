2e82960b9300718b051ae094370ef1526229ab30
myra-2512 <abhi.back2back@gmail.com>
2025-07-19 14:39:13 +0530

    Add files via upload

---
+++ conditionalhw.py
@@ -0,0 +1,8 @@
+x=int(input("Enter the temperature in Celsius: "))
+
+if x>25:
+    print("It's",x,"degrees Celsius,which is perfect weather to wear a T-shirt and shorts.Pheww!")
+else:
+    print("It's",x,"degrees Celsius,remember to to wear a jacket.Brrrr!")
+
+
